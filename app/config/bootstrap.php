4cf029ab3763b86773668d78cf512dadfb7f90e5
Miguel Nicolás <miguel.martinez@rsbmedia.com>
2020-11-11 13:40:03 +0100

    Instantiating Storage class

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,2 +1,13 @@
 <?php
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . './../../vendor/autoload.php';
+
+use App\Infrastructure\Storage\MemoryStorage;
+
+// Singleton functions
+// 
+
+function Storage()
+{
+
+	return MemoryStorage::getInstance();
+}
